8c95a1793f670e9fd4d1c9ee7a7944164a2ec1c3
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-18 21:05:34 +0200

    creating JS and CSS files and enqueue it

--- plugins/bookstore/bookstore.php
+++ plugins/bookstore/bookstore.php
@@ -67,3 +67,15 @@ function bookstore_add_isbn_to_quick_edit($keys, $post) {
     return $keys;
 }
 add_filter('postmeta_form_keys', 'bookstore_add_isbn_to_quick_edit', 10, 2);
+
+
+//Ejemplo para poner en cola archivos de JS o CSS
+function bookstore_enqueue_scripts() {
+    $post = get_post();
+    if('book' !== $post->post_type) {
+        return;
+    }
+    wp_enqueue_style('bookstore_style', plugins_url() . '/bookstore/bookstore.css');
+    wp_enqueue_script('bookstore_script', plugins_url() . '/bookstore/bookstore.js');
+};
+add_action('wp_enqueue_scripts', 'bookstore_enqueue_scripts');
